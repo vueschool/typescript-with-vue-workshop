a32f9c63bc6d5d0a81ce86760fb30ffb370b6ae3
Daniel Kelly <me@danielkelly.io>
2023-12-05 16:31:02 -0600

    wip

---
+++ components/AppModal.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+function open() {
+  console.log("opening modal");
+}
+
+defineExpose({ open });
+</script>
---
+++ examples/3-TemplateRef.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppModal from "@/components/AppModal.vue";
+
+const modal = ref<InstanceType<typeof AppModal>>();
+
+onMounted(() => {
+  if (!modal.value) return;
+  modal.value.open();
+});
+</script>
+
+<template>
+  <AppModal ref="modal" />
+</template>
