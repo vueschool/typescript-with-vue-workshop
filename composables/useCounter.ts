38b3d0fe7d71bae20863cf18cf2481ce6e8c4006
Daniel Kelly <me@danielkelly.io>
2023-12-05 21:31:47 -0600

    useCounter composable

---
+++ composables/useCounter.ts
@@ -0,0 +1,13 @@
+export const useCounter = () => {
+  const count = ref(1);
+  const doubled = computed(() => count.value * 2);
+  function increment() {
+    count.value++;
+  }
+
+  return {
+    count,
+    doubled,
+    increment,
+  };
+};
